ab81c2539fc9eefb9899b44d282bf72eab0a75de
DNSLV-PMTKV <pometkov.d@protonmail.com>
2020-08-20 16:18:21 +0300

    added message model

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,8 +1,22 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+from .models import Message
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
+
+    commands = {
+        'fetch_messages': fetch_messages,
+        'new_message': new_message
+    }
+
+    def fetch_messages(self, data):
+        pass
+
+    def new_message(self, data):
+        pass
+
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
